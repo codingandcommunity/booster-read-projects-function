67b2363c342881cfa607bf62d51c8cae65a85853
jakebillings <jake@jakebillings.com>
2018-10-09 15:19:19 -0400

    Add: dummy projects endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send({
+    name: 'Hello, World',
+    image_src: 'https://d1q6f0aelx0por.cloudfront.net/product-logos/5431a80b-9ab9-486c-906a-e3d4b5ccaa96-hello-world.png',
+    git_url: 'git@github.com:codingandcommunity/booster-project-hello-world.git'
+}));
 
 module.exports = app;
