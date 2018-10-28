35ee9c440e709be22a034cae2ee27b7afc5d7921
jakebillings <jake@jakebillings.com>
2018-10-28 13:11:10 -0400

    Tweak: serve projects from /projects using express instead of /

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send({
+app.get('/projects', (req, res) => res.send({
     name: 'Hello, World',
     image_src: 'https://d1q6f0aelx0por.cloudfront.net/product-logos/5431a80b-9ab9-486c-906a-e3d4b5ccaa96-hello-world.png',
     git_url: 'git@github.com:codingandcommunity/booster-project-hello-world.git'
