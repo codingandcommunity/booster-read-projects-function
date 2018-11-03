03e8919c7edeb091e877823de4a97484f2906c01
jakebillings <jake@jakebillings.com>
2018-11-02 20:52:07 -0400

    Tweak: return an array of projects on /projects instead of a project object

--- src/app.js
+++ src/app.js
@@ -11,10 +11,10 @@ app.use(function (req, res, next) {
 });
 
 
-app.get('/projects', (req, res) => res.send({
+app.get('/projects', (req, res) => res.send([{
     name: 'Hello, World',
     image_src: 'https://d1q6f0aelx0por.cloudfront.net/product-logos/5431a80b-9ab9-486c-906a-e3d4b5ccaa96-hello-world.png',
     git_url: 'git@github.com:codingandcommunity/booster-project-hello-world.git'
-}));
+}]));
 
 module.exports = app;
