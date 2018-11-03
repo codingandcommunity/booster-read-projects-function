ae8ac52021544a6f9f4119cbac97f124f05063f6
jakebillings <jake@jakebillings.com>
2018-11-02 20:38:27 -0400

    Add: CORS headers so that the react frontend can access it #4

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
+
+
 app.get('/projects', (req, res) => res.send({
     name: 'Hello, World',
     image_src: 'https://d1q6f0aelx0por.cloudfront.net/product-logos/5431a80b-9ab9-486c-906a-e3d4b5ccaa96-hello-world.png',
